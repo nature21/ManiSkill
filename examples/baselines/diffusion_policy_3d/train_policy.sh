a5cb21f7df35a49c65792e76334096a1f897e2aa
Ruoxiang LI <ruoxiang.li@my.cityu.edu.hk>
2025-01-15 02:13:59 +0800

    Update run_dir

--- examples/baselines/diffusion_policy_3d/train_policy.sh
+++ examples/baselines/diffusion_policy_3d/train_policy.sh
@@ -11,7 +11,7 @@ config_name=${alg_name}
 addition_info=${3}
 seed=${4}
 exp_name=${task_name}-${alg_name}-${addition_info}
-run_dir="~/.maniskill/data/outputs/${exp_name}_seed${seed}"
+run_dir="./data/outputs/${exp_name}_seed${seed}"
 
 
 # gpu_id=$(bash scripts/find_gpu.sh)
